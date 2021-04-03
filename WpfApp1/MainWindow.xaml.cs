a54184d181c65d9431c607433f2f2c6ad2c6e3ba
максим карбышев <79609799309@yandex.ru>
2021-04-03 16:59:22 +0700

    Добавленные комментарии, выделены регионы (в общем более удобное ориентирование в коде) и улучшена генерация поля

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,80 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Класс для реализация логики поля
     /// </summary>
     class Field
     {
         Random rnd = new Random();
-        public bool IsBomb { get; set; } = false;
-        public int BombCount { get; set; } = 45;
-        public void Generate(Field[,] field)
+        public bool IsBomb { get; set; } = false;       // Ячейка имеет состояние бомбы и нет
+        public int BombCount { get; set; } = 45;        // Количсетво бомб на поле
+        public void Generate(Field[,] field)            // Метод для генерации поля
         {
+            #region Генерация поля
             for (int i = 0; i < field.GetLength(1); i++)
             {
-                int count = rnd.Next(0, field.GetLength(0));
+                int count = rnd.Next(0, field.GetLength(0));                                // Получаем количество бомб на строку
                 for (int j = 0; j < count; j++)
                 {
-                    int temp = rnd.Next(0, field.GetLength(0));
-                    if (field[i, temp].IsBomb == true)
+                    int tempI = i;
+                    int temp = rnd.Next(0, field.GetLength(0));                             // Получаем положение бомбы
+                    if (field[i, temp].IsBomb)                                              // Если клетка уже занята бомбой, то
                     {
-                        bool isEnter = false;
-                        while (field[i, temp].IsBomb && temp < field.GetLength(0))
+                        temp = 0;                                                           // Проходим строку с начала и ищем место для бомбы
+                        while (field[i, temp].IsBomb && temp < field.GetLength(0))          
                         {
-                            if (!isEnter && temp == field.GetLength(0) - 1)
-                                temp = 0;
+                            if (temp == field.GetLength(0) - 1)
+                            {
+                                i++;                                                        // Если строка закончилась, то переходим на следующую
+                            }
                             temp++;
                         }
                     }
-                    field[i, temp].IsBomb = true;
-                    BombCount--;
-                    if (BombCount == 0)
+                    field[i, temp].IsBomb = true;                                           // Когда найдено место, устанавливаем бомбу
+                    BombCount--;                                                            // уменьшаем кол-во бомб
+                    i = tempI;                                                              // Возвращаемся на начальную строку
+                    if (BombCount == 0)                                                     // если все бомбы поставлены, то завершаем генерацию
                         break;
                 }
-                if (BombCount == 0)
+                if (BombCount == 0)                                                         // если все бомбы поставлены, то завершаем генерацию
                     break;
             }
-            if (BombCount > 0)
+            if (BombCount > 0)                                                              // если остались невыставленные бомбы, то вызываем метод снова
                 Generate(field);
+            #endregion
         }
     }
+    /// <summary>
+    /// Класс для для основного действа
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            Button button = new Button();
-            Field[,] field = new Field[10, 10];
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    field[i, j] = new Field();
-                }
-            }
-            Field fieldConstructor = new Field();
-            int bombCount = fieldConstructor.BombCount;
-            fieldConstructor.Generate(field);
+            Field[,] field = new Field[10, 10];                             // Создаём массив, размерностью поля
+            for (int i = 0; i < field.GetLength(0); i++)                
+            {                                                           //
+                for (int j = 0; j < field.GetLength(1); j++)            //
+                {                                                       // Заполняем его экземплярами класса
+                    field[i, j] = new Field();                          //
+                }                                                       //
+            }                                                           
+            Field fieldConstructor = new Field();                           // Делаем ещё экземпляр класса для взаимодействия с классом
+            int bombCount = fieldConstructor.BombCount;                 // Получаем количство бомб
+            fieldConstructor.Generate(field);                               // Вызываем генерацию поля
+            #region Пиздец
             if (field[0, 0].IsBomb)
                 C00.Content = '1';
             else
                 C00.Content = '0';
-            /*for (int i = 0; i < field.GetLength(0); i++)
+            for (int i = 0; i < field.GetLength(0); i++)
             {
                 for (int j = 0; j < field.GetLength(1); j++)
                 {
@@ -87,7 +84,8 @@ namespace WpfApp1
 
                     }
                 }
-            }*/
+            }
+            #endregion
         }
     }
 }
