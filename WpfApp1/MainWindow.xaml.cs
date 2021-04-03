27441ce521d1caf0024f151b15c4f93b7456ddcc
максим карбышев <79609799309@yandex.ru>
2021-04-03 21:12:52 +0700

    Добавлена иконка мины, и наконец сделанан проход по матрице кнопок

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 namespace WpfApp1
 {
     /// <summary>
@@ -9,40 +20,41 @@ namespace WpfApp1
     class Field
     {
         Random rnd = new Random();
-        public bool IsBomb { get; set; } = false;       // Ячейка имеет состояние бомбы и нет
-        public int BombCount { get; set; } = 45;        // Количсетво бомб на поле
+        public bool IsMine { get; set; } = false;       // Ячейка имеет состояние мины и нет
+        public int MineCount { get; set; } = 45;        // Количсетво мин на поле
         public void Generate(Field[,] field)            // Метод для генерации поля
         {
             #region Генерация поля
             for (int i = 0; i < field.GetLength(1); i++)
             {
-                int count = rnd.Next(0, field.GetLength(0));                                // Получаем количество бомб на строку
+                int count = rnd.Next(0, field.GetLength(0));                                // Получаем количество мин на строку
                 for (int j = 0; j < count; j++)
                 {
                     int tempI = i;
-                    int temp = rnd.Next(0, field.GetLength(0));                             // Получаем положение бомбы
-                    if (field[i, temp].IsBomb)                                              // Если клетка уже занята бомбой, то
+                    int temp = rnd.Next(0, field.GetLength(0));                             // Получаем положение мины
+                    if (field[i, temp].IsMine)                                              // Если клетка уже занята миной, то
                     {
-                        temp = 0;                                                           // Проходим строку с начала и ищем место для бомбы
-                        while (field[i, temp].IsBomb && temp < field.GetLength(0))          
+                        temp = 0;                                                           // Проходим строку с начала и ищем место для мины
+                        while (field[i, temp].IsMine && temp <= field.GetLength(0))
                         {
                             if (temp == field.GetLength(0) - 1)
                             {
                                 i++;                                                        // Если строка закончилась, то переходим на следующую
+                                temp = 0;
                             }
                             temp++;
                         }
                     }
-                    field[i, temp].IsBomb = true;                                           // Когда найдено место, устанавливаем бомбу
-                    BombCount--;                                                            // уменьшаем кол-во бомб
+                    field[i, temp].IsMine = true;                                           // Когда найдено место, устанавливаем мину
+                    MineCount--;                                                            // уменьшаем кол-во мин
                     i = tempI;                                                              // Возвращаемся на начальную строку
-                    if (BombCount == 0)                                                     // если все бомбы поставлены, то завершаем генерацию
+                    if (MineCount == 0)                                                     // если все мины поставлены, то завершаем генерацию
                         break;
                 }
-                if (BombCount == 0)                                                         // если все бомбы поставлены, то завершаем генерацию
+                if (MineCount == 0)                                                         // если все мины поставлены, то завершаем генерацию
                     break;
             }
-            if (BombCount > 0)                                                              // если остались невыставленные бомбы, то вызываем метод снова
+            if (MineCount > 0)                                                              // если остались невыставленные мины, то вызываем метод снова
                 Generate(field);
             #endregion
         }
@@ -56,18 +68,40 @@ namespace WpfApp1
         {
             InitializeComponent();
             Field[,] field = new Field[10, 10];                             // Создаём массив, размерностью поля
-            for (int i = 0; i < field.GetLength(0); i++)                
+            for (int i = 0; i < field.GetLength(0); i++)
             {                                                           //
                 for (int j = 0; j < field.GetLength(1); j++)            //
                 {                                                       // Заполняем его экземплярами класса
                     field[i, j] = new Field();                          //
                 }                                                       //
-            }                                                           
+            }
             Field fieldConstructor = new Field();                           // Делаем ещё экземпляр класса для взаимодействия с классом
-            int bombCount = fieldConstructor.BombCount;                 // Получаем количство бомб
+            int MineCount = fieldConstructor.MineCount;                 // Получаем количство мин
             fieldConstructor.Generate(field);                               // Вызываем генерацию поля
-            #region Пиздец
-            if (field[0, 0].IsBomb)
+            #region Жопа
+            //int m = 0, k = 0;
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"C:\Users\Максим\source\repos\Minesweeper\WpfApp1\Icons\Mine.jpg"));    // Выбираю изображение для мины
+            MineCounter.Text = Convert.ToString(MineCount - fieldConstructor.MineCount);                                    // Вывожу кол-во мин
+            foreach (UIElement item in FieldGrid.Children)                                              // Прохожу по матрице кнопок
+            {
+                for (int i = 0; i < field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < field.GetLength(1); j++)
+                    {
+                        if (Grid.GetRow(item) == i && Grid.GetColumn(item) == j)                        // Если Строка == i и столбец == j
+                        {
+                            if (field[i, j].IsMine)
+                            {
+                                ((Button)item).Content = image;
+                            }
+                            else
+                                ((Button)item).Content = '0';
+                        }
+                    }
+                }
+            }
+            /*if (field[0, 0].IsMine)
                 C00.Content = '1';
             else
                 C00.Content = '0';
@@ -75,7 +109,7 @@ namespace WpfApp1
             {
                 for (int j = 0; j < field.GetLength(1); j++)
                 {
-                    if (field[i, j].IsBomb)
+                    if (field[i, j].IsMine)
                     {
 
                     }
@@ -84,7 +118,7 @@ namespace WpfApp1
 
                     }
                 }
-            }
+            }*/
             #endregion
         }
     }
